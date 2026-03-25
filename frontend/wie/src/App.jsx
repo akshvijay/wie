0725e4633969248e07009d0c0494e61b3d7e98e6
akshita <akshi.vijay416@gmail.com>
2026-03-26 00:06:37 +0530

    .

--- frontend/wie/src/App.jsx
+++ frontend/wie/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Events from "./components/Events";
